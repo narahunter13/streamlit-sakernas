f3c59c54a4aa66a18c2566ab5e3fff3c4b215a93
narahunter13 <narahunter13@gmail.com>
2024-08-14 08:08:35 +0700

    Init

---
+++ app.py
@@ -0,0 +1,36 @@
+from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
+import streamlit as st
+import pandas as pd
+from json import loads
+from io import StringIO
+
+load_dotenv()
+
+supabase_url = os.getenv('SUPABASE_CLIENT_URL')
+supabase_key = os.getenv('SUPABASE_CLIENT_KEY')
+
+st.set_page_config(
+   page_title="Dashboard Sakernas",
+   page_icon="🧊"
+)
+
+# Create supabase client
+supabase: Client = create_client(supabase_url, supabase_key)
+
+# Create Title Element
+st.title('Dashboard Sakernas Agustus 2024 Pendataan Kota Lubuk Linggau')
+
+# Get data from database
+response = (
+    supabase.table("streamlit")
+    .select('*').single().execute()
+).model_dump_json()
+
+# Create dataframe
+json = StringIO(loads(response)['data']['data'])
+df = pd.read_json(json)
+
+st.header("Tabulasi Progres")
+st.dataframe(df, hide_index=True)
